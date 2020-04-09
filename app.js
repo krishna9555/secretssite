1e608c856e0968c174993fafafe0f8e5a2d910f6
krishna singh <ksdosad955@gmail.com>
2020-04-09 23:25:26 +0530

    using bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+// const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -21,7 +24,7 @@ const userSchema = new mongoose.Schema({
 
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
 
 //encrypt package is added as a plugin to the schema
 //mongoose encrypt will encrypt when save is called and will decrypt when find is called
@@ -44,9 +47,12 @@ app.get("/register", function(req, res){
 
 
 app.post("/register", function(req, res){
-	const newUser = new User({
+
+	bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+
+		const newUser = new User({
 		email: req.body.username,
-		password: req.body.password
+		password: hash
 	});
 	newUser.save(function(err){
 		if(!err){
@@ -55,6 +61,9 @@ app.post("/register", function(req, res){
 			console.log(err);
 		}
 	});
+        // Store hash in your password DB.
+    });
+
 });
 
 
@@ -67,9 +76,11 @@ app.post("/login", function(req, res){
 			console.log(err);
 		}else{
 			if(foundUser){
-				if(foundUser.password === password){
-					res.render("secrets");
-				}
+				bcrypt.compare(password, foundUser.password, function(err, result) {
+    				if(result === true){
+    					res.render("secrets");
+    				}
+				});
 			}
 		}
 	});
